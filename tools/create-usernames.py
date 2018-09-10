f45db5239c443b2b6ddace58e241fe4a7a0034a8
Manav <garg.manav8@gmail.com>
2018-09-10 11:58:42 +0530

    Create create-usernames.py

---
+++ tools/create-usernames.py
@@ -0,0 +1,6 @@
+# This script is able to create usernames that can be used for brute force attacks
+# from given names
+# Names can be provided from the command line or from a text file
+# Assumptions: usernames will only contain small alphabets with different combination of first
+# and last name
+
